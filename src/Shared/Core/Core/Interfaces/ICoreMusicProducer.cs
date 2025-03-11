3339d5e732ece1d3c8b1cb649bd939bd93cdbc04
Jack Caven <jack.r.caven@gmail.com>
2025-03-11 18:20:35 +0000

    feat(core): add configure method to core music producer

--- src/Shared/Core/Core/Interfaces/ICoreMusicProducer.cs
+++ src/Shared/Core/Core/Interfaces/ICoreMusicProducer.cs
@@ -1,13 +1,14 @@
-﻿using Core.DataStructures.Art;
+﻿using Core.BaseClasses;
+using Core.DataStructures.Art;
 using Core.DataStructures.Music;
 
 namespace Core.Interfaces
 {
     public interface ICoreMusicProducer
     {
+        public void Configure(MusicGeneratorConfiguration configuration);
         public MusicData Add(ObjectAttributes imageAttributes, CanvasAttributes canvasAttributes);
         public void Remove(ObjectAttributes imageAttributes, CanvasAttributes canvasAttributes);
-
         public void Clear();
     }
 }
